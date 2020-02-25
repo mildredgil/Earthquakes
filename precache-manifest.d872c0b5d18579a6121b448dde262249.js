3ad75bdd59ecaf274863b580c7c996ed0849d83b
Mildred Gil <mi.mitel@hotmail.com>
2020-02-25 01:18:46 -0600

    Updates

---
+++ precache-manifest.d872c0b5d18579a6121b448dde262249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbfe00c857b393d78be336e7cfb9c77",
+    "url": "/Earthquakes/index.html"
+  },
+  {
+    "revision": "cf3b593865a3aaa435aa",
+    "url": "/Earthquakes/static/css/main.ecb66ff4.chunk.css"
+  },
+  {
+    "revision": "5f60c07edbae1c1137df",
+    "url": "/Earthquakes/static/js/2.db88b51e.chunk.js"
+  },
+  {
+    "revision": "01614b5008b8342eab2dca154aea9eb9",
+    "url": "/Earthquakes/static/js/2.db88b51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3b593865a3aaa435aa",
+    "url": "/Earthquakes/static/js/main.b298da58.chunk.js"
+  },
+  {
+    "revision": "d39c84019c3508f069d8",
+    "url": "/Earthquakes/static/js/runtime-main.fa72c26c.js"
+  }
+]);
